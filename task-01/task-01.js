7e0f8b53c199dda17516af0cb2cb0f14d734ac74
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-05-29 14:42:51 +0200

    e

--- task-01/task-01.js
+++ task-01/task-01.js
@@ -1,4 +1,9 @@
-console.clear(0);
+console.clear();
 
 const categoriesNumber = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categoriesCount.length}`);
+console.log(`Number of categories: ${categoriesNumber.length}`);
+
+const categoriesName = element.querySelectorAll("h2");
+const categoriesElements = element.querySelectorAll("li");
+console.log(`Category: ${categoriesName.textContent}`);
+console.log(`Elements: ${categoriesElements.length}`);

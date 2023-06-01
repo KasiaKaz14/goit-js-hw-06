49a1226e200cba873bf62b78703944682eb1e439
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2023-06-01 12:32:56 +0200

    e

--- task-09/task-09.js
+++ task-09/task-09.js
@@ -1,18 +1,15 @@
 const widget = document.querySelector(".widget");
 const color = document.querySelector(".color");
-const changeColor = document.querySelector(".change-color");
-
-const body = changeColor;
+const body = document.querySelector(".change-color");
 
 function getRandomHexColor() {
-  if (
-    document.getElementById("body").backgroundColor.value ===
-    backgroundColor.value
-  ) {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function setNewColor() {
+  body.style.backgroundColor = `$(getRandomHexColor.value)`;
 }
 
 changeColor.addEventListener("change", getRandomHexColor);
